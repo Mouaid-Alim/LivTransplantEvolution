08a158b41bf14b76e76206c047f829f3a2805475
Mouaid-Alim <mouaidalim1@gmail.com>
2023-12-10 05:47:30 -0500

    Updated the preapre data fucntion to reflect the pivot towards taking SRTR stathiist_liin datafile as input in csv form. Updated the documentation to make it more thorough and added a working example. Made the refernces accurate and more thorough as well. reflected these cvhanges in the man and description files

--- R/prepareData.R
+++ R/prepareData.R
@@ -1,4 +1,5 @@
-#' Process raw patient data and extract relevant biomarkers
+#' Process raw patient SRTR data stathiist_liin data file and extract relevant
+#'  biomarkers
 #'
 #' This function Process data from the STATHIIST_LIIN data file from the SRTR
 #' transplantation dataset. The data must be in csv format.
@@ -40,13 +41,46 @@
 #' @import readr
 #' @import dplyr
 #' @examples
-#' #Example 1
-#' prepareDate(../data/liver_data)
-#'
+#' # Example 1
+#' # provide the prepareData function with the path to a csv folder containg the
+#' # data and it will return a dataframe with the relevant data
+#' \dontrun{
+#' cleanedData = prepareData(../inst//extdata/updated_meld_patient_data.csv)
+#'}
 #' @references
-#' UCI Machine Learning. (2017). Indian Liver Patient Records [Data set].
+#' The SRTR database.
+#'     Srtr.org https://www.srtr.org/about-the-data/the-srtr-database/.
+#'
+#' Kamath, P. et al. A model to predict survival in patients with end-stage
+#'     liver disease. Gastroenterology 120, A76–A77 (2001).
+#'
+#' Kim, W. R. et al. MELD 3.0: The model for end-Stage Liver Disease updated for
+#'      the modern era. Gastroenterology 161, 1887-1895.e4 (2021).
+#'
+#' Kwong, A. J. et al. OPTN/SRTR 2019 annual data report: Liver. Am. J.
+#'     Transplant 21 Suppl 2, 208–315 (2021).
+#'
+#' Slack, A., Yeoman, A. & Wendon, J. Renal dysfunction in chronic liver
+#'     disease. Crit. Care 14, 214 (2010).
+#'
+#' Biggins, S. W. et al. Serum sodium predicts mortality in patients listed for
+#'     liver transplantation. Hepatology 41, 32–39 (2005).
+#'
+#' Guerra Ruiz, A. R. et al. Measurement and clinical usefulness of bilirubin in
+#'      liver disease. Adv. Lab. Med. 2, 352–361 (2021).
+#'
+#' Kalas, M. A., Chavez, L., Leon, M., Taweesedt, P. T. & Surani, S. Abnormal
+#'     liver enzymes: A review for clinicians. World J. Hepatol.
+#'     13, 1688–1698 (2021).
+#'
+#' Tufoni, M., Zaccherini, G., Caraceni, P. & Bernardi, M. Albumin: Indications
+#'     in chronic liver disease. United European Gastroenterol. J.
+#'     8, 528–535 (2020).
+#'
+#'
 #'
 prepareData <- function(filePath) {
+  # install reader package if it is not already installed
   if (!requireNamespace("readr", quietly = TRUE)) {
     install.packages("readr")
     library(readr)
@@ -54,9 +88,52 @@ prepareData <- function(filePath) {
 
   data <- readr::read_csv(filePath)
 
+  # Input Checks
+  # check if all coloumns present
+  if (!("CANHX_BEGIN_DT" %in% colnames(data))) {
+    stop("Your dataset does not have the CANHX_BEGIN_DT column, make sure it does
+         before running this function")
+  }
+  if (!("CANHX_ALBUMIN" %in% colnames(data))) {
+    stop("Your dataset does not have the CANHX_ALBUMIN column, make sure it does
+         before running this function")
+  }
+  if (!("CANHX_BILI" %in% colnames(data))) {
+    stop("Your dataset does not have the CANHX_BILI column, make sure it does
+         before running this function")
+  }
+  if (!("CANHX_SERUM_CREAT" %in% colnames(data))) {
+    stop("Your dataset does not have the CANHX_SERUM_CREAT column, make sure it
+         does before running this function")
+  }
+  if (!("CANHX_INR" %in% colnames(data))) {
+    stop("Your dataset does not have the CANHX_INR column, make sure it does
+         before running this function")
+  }
+  if (!("CANHX_SERUM_SODIUM" %in% colnames(data))) {
+    stop("Your dataset does not have the CANHX_SERUM_SODIUM column, make sure it
+         does before running this function")
+  }
+  if (!("CANHX_DIAL_PRIOR_WEEK" %in% colnames(data))) {
+    stop("Your dataset does not have the CANHX_DIAL_PRIOR_WEEK column, make sure
+         it does before running this function")
+  }
+  if (!("CAN_GENDER" %in% colnames(data))) {
+    stop("Your dataset does not have the CAN_GENDER column, make sure it does
+         before running this function")
+  }
+
+  # Extract Relevant columns to create new data frame
   selectedData <- data %>%
-    dplyr::select(Date, Sex, DialysisTwiceInPastWeek, Creatinine, INR,
-                  Total_Bilirubin, Sodium, Albumin)
+    dplyr::select(PX_ID, CANHX_BEGIN_DT, CANHX_ALBUMIN, CANHX_BILI, CANHX_INR,
+                  CANHX_SERUM_CREAT, CANHX_SERUM_SODIUM, CAN_GENDER,
+                  CANHX_DIAL_PRIOR_WEEK)
+  # remove rows with al na values
   selectedData[rowSums(is.na(selectedData)) != ncol(selectedData),]
+
+  #rename columns to be more intuitive
+  colnames(selectedData) <- c('Patient_ID', 'Data', 'Albumin', 'Bilirubin', 'INR',
+                           'Serum_Creatinine', 'Serum_Sodium', 'Gender',
+                           'Dialysis_In_Past_Week')
   return(selectedData)
 }
